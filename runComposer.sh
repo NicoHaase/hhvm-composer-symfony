0ef565cd58bfbff59c4fde390361bcb4bdee75fe
Nico Haase <nico@nicohaase.de>
2016-03-31 11:36:55 +0200

    Set proper permissions

--- runComposer.sh
+++ runComposer.sh
@@ -3,5 +3,6 @@
 composer.phar self-update
 composer.phar update
 chown -R $DOCKERUSER:$DOCKERUSER app vendor bin public/bundles composer.lock
-chmod -R 777 app/ vendor/ bin/ public/bundles/ composer.lock
+chmod -R g+w app/ vendor/ bin/ public/bundles/ composer.lock
 
+chown -R $DOCKERUSER:$DOCKERUSER /root/.composer/
